cd4aaa77f5c378a2e8702c7e70fd9e67d924ef83
OliverJesmon <102456180+OliverJesmon@users.noreply.github.com>
2024-03-11 18:34:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ class ChatWindow(QMainWindow):
         self.txt = QLabel("Howdy!Welcome to the chatroom")
         self.layout.addWidget(self.txt)
 
-        self.txt1 = QTextEdit()
-
+        self.txt1 = QTextEdit() #Text view
         self.txt1.setReadOnly(True)
         self.layout.addWidget(self.txt1)
         
@@ -43,7 +42,7 @@ class ChatWindow(QMainWindow):
         self.tplace = QTextEdit()
         self.tplace.setFixedHeight(70)
         self.layout.addWidget(self.tplace)
-        #button
+        #send button
         self.sendButton = QPushButton('Send')
         self.sendButton.clicked.connect(self.send)
         self.sendButton.setFixedSize(45,45)
@@ -51,25 +50,25 @@ class ChatWindow(QMainWindow):
         #Menubar
         self.menuBar = self.menuBar()
         fileMenu = self.menuBar.addMenu('File')
-        endChatAction = QAction('End Chat', self)
+        endChatAction = QAction('End Chat', self) #ends the conversation with user
         endChatAction.triggered.connect(self.end_chat)
         fileMenu.addAction(endChatAction)
-        exitAction = QAction('Exit', self)
+        exitAction = QAction('Exit', self) #closes the desktop
         exitAction.triggered.connect(self.close)
         fileMenu.addAction(exitAction)
 
     def send(self):
         user_input = self.tplace.toPlainText().strip()
-        if not user_input:
+        if not user_input:#if clicked with void text box
             return
         letter = f"{self.user}: {user_input}\n"
         self.txt1.append(letter)
-        self.users.send(letter.encode('utf-8'))
+        self.users.send(letter.encode('utf-8'))#sends the message to other users in BIN format
         self.tplace.clear()
 
     def end_chat(self):
         
-        self.users.send(self.terminate.encode('utf-8'))
+        self.users.send(self.terminate.encode('utf-8')) #sends "an individual has left the chat"
         self.txt1.setReadOnly(True)
         try:
           self.users.close() #termination of user connection 
